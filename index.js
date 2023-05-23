f67e9a647693161075d7300f32bd6aae9594cb8c
saifsammy <saifsammy653@gmail.com>
2023-05-23 23:05:48 +0600

    mongoDB client user name and password added on env file

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.use(cors());
 app.use(express.json());
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h290xzo.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h290xzo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 // mongoDB compass
 // const uri = 'mongodb://0.0.0.0:27017/'
